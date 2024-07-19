37baf2de086eab3e7185785f06a798103d86ce7f
betocostadev <robertomarx@gmail.com>
2024-07-19 16:56:41 -0300

    Added image test with Vite

--- Vite/vite-starter/src/index.js
+++ Vite/vite-starter/src/index.js
@@ -1,5 +1,12 @@
 // import { initializeCounter } from './counter'
 
+import image from './steve-after-a-workshop.jpg'
+
+const img = document.createElement('img')
+img.src = image
+
+document.querySelector('#content').appendChild(img)
+
 import('./counter').then(({ initializeCounter }) => {
   initializeCounter()
 })
