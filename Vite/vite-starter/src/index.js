193aa212348f2bdf56cc08d5dc1ecf9a7a88be2d
betocostadev <robertomarx@gmail.com>
2024-07-20 16:52:57 -0300

    Added some imports and image tools to vite-starter

--- Vite/vite-starter/src/index.js
+++ Vite/vite-starter/src/index.js
@@ -1,11 +1,15 @@
 // import { initializeCounter } from './counter'
 
-import image from './steve-after-a-workshop.jpg'
+import image from './steve-after-a-workshop.jpg?h=600&format=webp'
 
 const img = document.createElement('img')
 img.src = image
 
 document.querySelector('#content').appendChild(img)
+
+console.log(import.meta)
+console.log('Getting the env')
+console.log(import.meta.env)
 
 import('./counter').then(({ initializeCounter }) => {
   initializeCounter()
@@ -14,3 +18,12 @@ import('./counter').then(({ initializeCounter }) => {
 console.log('Hello from index.js')
 
 document.querySelector('h2').textContent = 'Hello from index.js'
+
+// JSON Named Exports
+
+// import gandorf from './characters/gandorf.json'
+// It exports every key as a single const
+// At build it will tree shake what is not used.
+import { name } from './characters/gandorf.json'
+
+console.log(`Gandorfs name from the json is: ${name}`)
--- Vite/vite-starter/vite.config.js
+++ Vite/vite-starter/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
+  plugins: [imagetools()],
   css: {
     postcss: {
       map: true,
