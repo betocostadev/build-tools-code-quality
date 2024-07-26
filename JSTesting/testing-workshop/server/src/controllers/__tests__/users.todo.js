ddafe13443123594379829bd8a353fd68a78551d
betocostadev <robertomarx@gmail.com>
2024-07-26 10:52:20 -0300

    Finished tests for server users mock

--- JSTesting/testing-workshop/server/src/controllers/__tests__/users.todo.js
+++ JSTesting/testing-workshop/server/src/controllers/__tests__/users.todo.js
@@ -1,3 +1,30 @@
+import {omit} from 'lodash'
+import {initDb} from 'til-server-test-utils'
+import * as usersController from '../users'
+import db from '../../utils/db'
+
+const safeUser = (u) => omit(u, ['salt', 'hash'])
+
+beforeEach(() => initDb())
+
+test('getUsers returns all users in the database', async () => {
+  const req = {}
+  const res = {
+    json: jest.fn(),
+  }
+
+  await usersController.getUsers(req, res)
+
+  expect(res.json).toHaveBeenCalledTimes(1)
+  const firstCall = res.json.mock.calls[0]
+  // console.log(firstCall)
+  const firstArg = firstCall[0]
+  const {users} = firstArg
+  expect(users.length).toBeGreaterThan(0)
+  const actualUsers = await db.getUsers()
+  expect(users).toEqual(actualUsers.map(safeUser))
+})
+
 //////// Elaboration & Feedback /////////
 // When you've finished with the exercises:
 // 1. Copy the URL below into your browser and fill out the form
